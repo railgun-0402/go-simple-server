09cf499109580a3eb7e9f53e1e9d28446b191f7d
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-10-19 19:08:32 +0900

    Create a simple server

---
+++ server/server.go
@@ -0,0 +1,63 @@
+package server
+
+import (
+	"log"
+	"net/http"
+	"sync/atomic"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	counter int64
+
+	server *http.Server
+	router *gin.Engine
+}
+
+func New() *Server {
+	router := gin.Default()
+	server := &Server {
+		router: router,
+		counter: int64(0),
+	}
+
+	router.GET("/", server.CounterHandler)
+	router.GET("/health_checks", server.HealthHandler)
+
+	return server
+}
+
+// 成功レスを返却するだけ
+func (s *Server) HealthHandler(ctx *gin.Context) {
+	ctx.JSON(200, gin.H{"success": true})
+}
+
+// リクエスト数を計算
+func (s *Server) CounterHandler(ctx *gin.Context) {
+	counter := atomic.AddInt64(&s.counter, 1)
+	ctx.JSON(200, gin.H{"counter": counter})
+}
+
+// サーバ起動・引数アドレスをリッスン
+func (s *Server) Start(address string) error {
+	s.server = &http.Server{
+		Addr: address,
+		Handler: s.router,
+		ReadTimeout: 10 * time.Second,
+	}
+	log.Printf("start server on %s", address)
+
+	return s.server.ListenAndServe()
+}
+
+// サーバを停止
+func (s *Server) Stop() error {
+	log.Printf("stop server")
+	if s.server != nil {
+		return s.server.Close()
+	}
+
+	return nil
+}
